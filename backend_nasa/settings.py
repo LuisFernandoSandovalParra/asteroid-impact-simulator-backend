3594105158b974a2307ce9ed492ce8407a74ff6f
lufsando <lufsando@bancolombia.com.co>
2025-10-06 09:44:32 -0500

    Add and fix files to deployment

--- backend_nasa/settings.py
+++ backend_nasa/settings.py
@@ -130,6 +130,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000", 
     "http://127.0.0.1:8000",
+    "https://asteroid-impact-simulator.up.railway.app/"
 ]
 
 # Deshabilitar CSRF para APIs (solo desarrollo)
